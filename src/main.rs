93f269f3aa9a1b6b675314a6c21c05f512d2279b
MovaDev <dev@mova.club>
2024-07-14 01:31:59 +0300

    Run two servers

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use proto::database::{GetHashRecordRequest, GetHashRecordResponse, HashDB};
+use proto::{
+    database_server::{Database, DatabaseServer},
+    kektor_server::{Kektor, KektorServer},
+    GetRecordRequest, GetRecordResponse, KektorRequest, KektorResponse,
+};
+use tokio::runtime::Runtime;
 use tonic::{transport::Server, Request, Response, Status};
 
 mod proto {
@@ -6,35 +11,90 @@ mod proto {
 }
 
 #[derive(Debug, Default)]
-struct HashDBService {}
+struct DatabaseService {}
 
 #[tonic::async_trait]
-impl HashDB for HashDBService {
-    fn get_record(
+impl Database for DatabaseService {
+    async fn get_record(
         &self,
-        request: Request<GetHashRecordRequest>,
-    ) -> Result<Response<GetHashRecordResponse>, Status> {
-        println!("Get new request: {:?}", request);
+        request: Request<GetRecordRequest>,
+    ) -> Result<Response<GetRecordResponse>, Status> {
+        println!("Get new request: {:#?}", request);
 
         let req = request.into_inner();
-        let reply = GetHashRecordResponse {
-            key: "kek",
-            value: b"lol",
+        println!("Inner object: {:#?}", req);
+
+        let reply = GetRecordResponse {
+            key: "kek".to_string(),
+            value: b"lol".to_vec(),
         };
 
-        Ok(Response::new(reply));
+        Ok(Response::new(reply))
+    }
+}
+
+#[derive(Debug, Default)]
+struct KektorService {}
+
+#[tonic::async_trait]
+impl Kektor for KektorService {
+    async fn kek(
+        &self,
+        request: Request<KektorRequest>,
+    ) -> Result<Response<KektorResponse>, Status> {
+        let req = request.into_inner();
+        println!("Kektor req: {:#?}", req);
+
+        Ok(Response::new(KektorResponse {
+            val: "Hello, from kektor!".to_string(),
+        }))
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse()?;
-    let server = HashDBService::default();
+    let addr1 = "[::1]:50051".parse()?;
+    let addr2 = "[::1]:50052".parse()?;
+    let db_service = DatabaseService::default();
+    let kek_service = KektorService::default();
 
-    Server::builder()
-        .add_service(HashDBService::new(server))
-        .serve(addr)
-        .await?;
+    let th1 = std::thread::spawn(move || {
+        let rt1 = Runtime::new().unwrap();
+        rt1.block_on(async move {
+            Server::builder()
+                .add_service(DatabaseServer::new(db_service))
+                .serve(addr1)
+                .await
+                .unwrap()
+        });
+    });
+
+    let th2 = std::thread::spawn(move || {
+        let rt2 = Runtime::new().unwrap();
+        rt2.block_on(async move {
+            Server::builder()
+                .add_service(KektorServer::new(kek_service))
+                .serve(addr2)
+                .await
+                .unwrap()
+        });
+    });
+
+    th1.join().unwrap();
+    th2.join().unwrap();
 
     Ok(())
 }
+
+// #[tokio::main]
+// async fn main() -> Result<(), Box<dyn std::error::Error>> {
+//     let addr = "[::1]:50051".parse()?;
+//     let service = DatabaseService::default();
+
+//     Server::builder()
+//         .add_service(DatabaseServer::new(service))
+//         .serve(addr)
+//         .await?;
+
+//     Ok(())
+// }
